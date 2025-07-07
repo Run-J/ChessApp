18aed8223d6fe135ae44a1d715ed16cbd2a644c4
Run-J <jirun040403@hotmail.com>
2025-07-08 00:39:38 +0800

    Apply the useChessStore to my ChessBoard.tsx

--- components/ChessBoard.tsx
+++ components/ChessBoard.tsx
@@ -1,10 +1,11 @@
-import { Chess } from 'chess.js'; // 控制棋局状态（走法合法性、轮次、胜负判断）
-import { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { useChessStore } from '@/stores/useChessStore';
+import { Alert, StyleSheet, View } from 'react-native';
 import Chessboard from 'react-native-chessboard'; // UI 上显示棋盘，响应用户点击
 
+
 export default function ChessBoard() {
-  const [game, setGame] = useState(new Chess()); // 创建一个新的棋局
+  const fen = useChessStore((state) => state.fen);
+  const makeMove = useChessStore((state) => state.makeMove);
 
   // 当用户点击棋盘上的棋子或格子
   const onMove = (info: any) => {
@@ -20,30 +21,17 @@ export default function ChessBoard() {
       return;
     }
 
-    const gameCopy = new Chess(game.fen()); // 创建棋局副本，防止直接修改原game对象的状态
+    const success = makeMove ({ from, to, promotion});
 
-    const moveParams: any = { from, to };
-
-    // 如果 UI 升变弹窗中用户选择了 promotion（例如 'q', 'r', 'b', 'n'）
-    // 就将其加入 move 参数，否则不能强行加，会造成非法走法
-    if (promotion) {
-      moveParams.promotion = promotion;
-    }
-
-    const move = gameCopy.move(moveParams);
-
-    if (move) {
-      setGame(gameCopy); // 合法走法，更新状态
-    } else {
-      console.log('非法走法！');
-      alert("非法走棋");
+    if (!success) {
+      Alert.alert("非法走定");
     }
   };
 
   return (
     <View style={styles.container}>
       <Chessboard 
-        fen={game.fen()} // 显示当前棋盘状态
+        fen={fen} // 显示当前棋盘状态 来自store
         onMove={onMove} // 用户点击后触发
       />
     </View>
--- stores/useChessStore.ts
+++ stores/useChessStore.ts
@@ -4,6 +4,7 @@ import { create } from 'zustand';
 type Move = {
     from: string;
     to: string;
+    promotion? : 'q' | 'r' | 'b' | 'n';
 }
 
 interface ChessState {
@@ -20,18 +21,23 @@ export const useChessStore = create<ChessState>((set, get) => ({
     fen: new Chess().fen(),
     turn: 'w',
     moves: [],
-    makeMove: ( { from, to } ) => {
-        const game = new Chess(get().fen);
-        const result = game.move({from, to, promotion: 'q'});
+    makeMove: ({ from, to, promotion }) => {
+        const game = new Chess(get().fen); // 获得副本
+        const moveObj: any = { from, to };
+        // 如果 UI 升变弹窗中用户选择了 promotion（例如 'q', 'r', 'b', 'n'）
+        // 就将其加入 moveObj 参数，否则不能强行加，会造成非法走法
+        if (promotion) {
+            moveObj.promotion = promotion;
+        }
 
+        const result = game.move(moveObj); // move 副本
         if (result) {
-            set ({
-                game,
+            set({
+                game, // 如果可以move，才会更新store里的Chess状态为最新的副本game
                 fen: game.fen(),
                 turn: game.turn(),
-                moves: [...get().moves, {from, to}],
+                moves: [...get().moves, { from, to }],
             });
-
             return true;
         }
         return false;
